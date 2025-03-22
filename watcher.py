4839bae0647227d9a8596f456224a5594af8e1aa
Waghddoh <mandarrajbhar6@gmail.com>
2025-03-22 21:08:40 +0530

    watcher.py

--- watcher.py
+++ watcher.py
@@ -5,7 +5,7 @@ from aiogram import Bot
 import asyncio
 
 API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+ADMIN_ID = '8027686324'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
